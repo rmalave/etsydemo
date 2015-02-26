e5b005e5c62686e2f552d8572c5e24a992e5736b
Rafael Malave <rafomalave@gmail.com>
2015-02-26 08:27:40 -0800

    Finish Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,14 @@
 class Listing < ActiveRecord::Base
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>"  }, :default_url => "default-placeholder.png",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :dropbox_options => {}
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+    if Rails.env.development?
+        has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>"  }, :default_url => "default-placeholder.png",
+        validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    else 
+        has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>"  }, :default_url => "default-placeholder.png",
+                          :storage => :dropbox,
+                          :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                          :dropbox_options => {}
+                          :path => ":style/:id_filename"
+        validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    end
 end
