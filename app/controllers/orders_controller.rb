0fa270f6eced63863f682e2db963bbb903e8b239
Rafael Malave <rafomalave@gmail.com>
2015-02-28 18:15:09 -0800

    Set up Transfers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,25 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+        charge = Stripe::Charge.create(
+            :amount => (@listing.price * 100).floor,
+            :currency => "usd",
+            :card => token
+        )
+    rescue Stripe::CardError => e
+        flash[:danger] = e.message
+    end
+
+    transfer = Stripe::Transfer.create(
+        :amount => ( @listing.price * 95 ).florr,
+        :currency => "usd",
+        :recipient => @seller.recipient
+    )
+
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_url, notice: 'Order was successfully created.' }
